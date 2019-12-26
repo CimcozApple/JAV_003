c73ecea540522e1dc4285c41a22510db13762b60
Yan <yan.bondarenko@gmail.com>
2019-12-26 23:59:47 +0200

    Challenge to decide new 54th pyramid

--- src/Pattern_054.java
+++ src/Pattern_054.java
@@ -1,23 +1,77 @@
+//public class Pattern_054 {
+//    public static void main(String[] args) {
+//        /*
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
+//        * * * * * *
+//        * * * * *
+//        * * * *
+//        * * *
+//        * *
+//        *
+//        *
+//        * 1, 3, 5, 7...
+//        * */
+//        int heightOfPyramid = 3;
+//        int b = 1;
+//
+//        for (int i = 0; i < heightOfPyramid; i++) {
+//            for (int a = 0; a < b; a++) {
+//                System.out.print("* ");
+//            }
+//
+//            b++;
+//            System.out.println();
+//
+//            for (int c = 3 - 1; c == b; c--) {
+//                System.out.print("* ");
+//            }
+//
+//            b--;
+//            System.out.println();
+//        }
+//    }
+//}
+
 public class Pattern_054 {
     public static void main(String[] args) {
         /*
-        *
-        * *
-        * * *
-        * * * *
-        * * * * *
-        * * * * * *
-        * * * * *
-        * * * *
-        * * *
-        * *
-        *
-        *
-        * */
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+         *
+         * 1, 2, 3...
+         * */
         int heightOfPyramid = 3;
+        int strong = heightOfPyramid * 2 - 1;
+        int b = 1;
 
-        for (int i = 0; i < heightOfPyramid; i++) {
+        for (int i = 0; i < strong; i++) {
+            for (int a = 0; a < b; a++) {
+                System.out.print("* ");
+            }
 
+            b++;
+//            System.out.println();
+
+            for (int c = 0; c < b; c++) {
+                System.out.println();
+                System.out.print("* ");
+                b--;
+            }
+
+            System.out.println();
         }
     }
 }
