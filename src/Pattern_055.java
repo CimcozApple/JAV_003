ed46878e3e6f2b34dc5a2416e98991b54a2d6202
Yan <yan.bondarenko@gmail.com>
2019-12-29 23:41:18 +0200

    Start 55th pattern

---
+++ src/Pattern_055.java
@@ -0,0 +1,16 @@
+public class Pattern_055 {
+    public static void main(String[] args) {
+        /*
+        * 4
+        * 4 3
+        * 4 3 2
+        * 4 3 2 1
+        * 4 3 2 1 0
+        * 4 3 2 1
+        * 4 3 2
+        * 4 3
+        * 4
+        * */
+
+    }
+}
